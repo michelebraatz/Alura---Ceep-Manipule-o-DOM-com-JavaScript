4cff8e4623f6624f9e92e4cb45f16bfb03f6340c
Michele Braatz <mbraaatz@gmail.com>
2020-08-10 20:01:46 -0300

    Comportamento do formulário e imprimindo no console.log

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,15 +1,19 @@
+//função
+const criarTarefa = (evento) => {
+    //evento
+    evento.preventDefault();    
+
+    //input
+    const input = document.querySelector('[data-form-input]');
+    //Capturando valor do input
+    const valor = input.value;
+    
+    console.log (valor)
+    input.value = " "
+}
+
 //button
 const novaTarefa = document.querySelector('[data-form-button]');
 
-
 // Escutando evento
-novaTarefa.addEventListener('click', ()=> {
-
-//input
-const input = document.querySelector('[data-form-input]');
-  
-//Capturando valor do input
-const valor = input.value;
-    
-    console.log (valor)
-});
+novaTarefa.addEventListener('click', criarTarefa);
