68e036308ce7b61ae04e99db433a7a838fa8be1c
Michele Braatz <mbraaatz@gmail.com>
2020-08-10 22:35:40 -0300

    Organizando Cód

---
+++ Componentes/deletaTarefa.js
@@ -0,0 +1,24 @@
+//função deletar
+const BotaoDeleta = (evento) => {
+    //criar botão deleta
+    const botaoDeleta = document.createElement('button');
+
+    //colocar nome no botão
+    botaoDeleta.innerHTML = 'Deletar' 
+    
+    //evento click e função
+    botaoDeleta.addEventListener('click', deletarTarefa);
+
+    return botaoDeleta;
+                
+}
+// deletar
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target
+    
+    const tarefaCompleta = botaoDeleta.parentElement
+
+    tarefaCompleta.remove()
+
+    return botaoDeleta;
+}
